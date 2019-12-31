d98895291953302a30a3cf1d19bfbe4ccde4624d
htanimotoHT <h1tanimotohiroshi@gmail.com>
2019-12-31 15:37:14 +0900

    firebaseを使用して、登録から編集まで

--- src/main.js
+++ src/main.js
@@ -3,8 +3,24 @@ import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBG3KIvcMelCmXa6RVgQPN6T8SnRVa96Zk",
+  authDomain: "address-vue.firebaseapp.com",
+  databaseURL: "https://address-vue.firebaseio.com",
+  projectId: "address-vue",
+  storageBucket: "address-vue.appspot.com",
+  messagingSenderId: "1005597005487",
+  appId: "1:1005597005487:web:dd6a66f2232f4049a3665a",
+  measurementId: "G-4P0ECSM0V4"
+};
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 
 new Vue({
   router,
